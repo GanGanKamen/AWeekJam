b7f598cb6acb8fe1657f73fe3219b0271e9764ff
GanGanKamen <m01171408c@edu.teu.ac.jp>
2019-07-02 10:03:02 +0900

    バナナ～ぬいぐるみ、7コの単語でマジカルバナナできるようにした

---
+++ MagicalBanana/Assets/GanGanKamen/Scripts/Date.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Date : MonoBehaviour
+{
+    public string name;
+    public bool canSelect;
+    public bool isSelected;
+    public List<Date> nextdates;
+    private Button button;
+    [SerializeField] private GameObject selectedMark;
+    [SerializeField] private Text nameText;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+    }
+
+    void Start()
+    {
+        nameText.text = name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ButtonStatus();
+    }
+
+    private void ButtonStatus()
+    {
+        if(canSelect == true)
+        {
+            button.interactable = true;
+        }
+        else
+        {
+            button.interactable = false;
+        }
+        if(isSelected == true)
+        {
+            selectedMark.SetActive(true);
+        }
+        else
+        {
+            selectedMark.SetActive(false);
+        }
+    }
+}
---
+++ MagicalBanana/Assets/GanGanKamen/Scripts/MagicalSet.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicalSet : MonoBehaviour
+{
+    public List<Date> dates;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            dates.Add(transform.GetChild(i).GetComponent<Date>());
+        }
+        for(int i = 0; i < dates.Count; i++)
+        {
+            dates[i].canSelect = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DateSelected(Date thisDate)
+    {
+        if(thisDate.isSelected == true||thisDate.canSelect == false)
+        {
+            return;
+        }
+        thisDate.isSelected = true;
+        for(int i = 0; i < dates.Count; i++)
+        {
+            dates[i].canSelect = false;
+        }
+        for (int i = 0; i < thisDate.nextdates.Count; i++)
+        {
+            if(thisDate.nextdates[i].isSelected == false)
+            {
+                thisDate.nextdates[i].canSelect = true;
+            }
+            
+        }
+    }
+}
